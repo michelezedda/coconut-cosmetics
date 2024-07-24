3c541682f9ee02865d7893494fd03431864ca955
Michele <mikzed@hotmail.it>
2024-07-24 14:01:31 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet";
+
+function App() {
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>coconut. cosmetics</title>
+      </Helmet>
+    </>
+  );
+}
+
+export default App;
