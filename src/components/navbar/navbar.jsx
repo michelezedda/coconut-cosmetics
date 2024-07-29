e26d9178ba3306db7ef05a0300c23a51ca0472ec
Michele <mikzed@hotmail.it>
2024-07-29 16:59:48 +0200

    navbar created

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,26 @@
 import myLogo from "/logo.png";
 import "./navbar.css";
 import { Link } from "react-router-dom";
+import { TbShoppingCart } from "react-icons/tb";
 
 function Navbar() {
   return (
     <>
-      <div className="navbar-container">
-        <div className="logo-container">
-          <Link to="/">
-            <img src={myLogo} alt="coconut-cosmetics logo" />
-          </Link>
-        </div>
-      </div>
+      <nav>
+        <Link to="/">
+          <img src={myLogo} alt="coconut-cosmetics logo" />
+        </Link>
+        <ul className="menu">
+          <li>
+            <a>categories</a>
+          </li>
+          <li>
+            <a>
+              <TbShoppingCart />
+            </a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
