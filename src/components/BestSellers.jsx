3fcd2cc75b859ab0b9c3a11f3549efbc874b0645
Michele <mikzed@hotmail.it>
2024-08-04 13:33:52 +0200

    products list added

--- src/components/BestSellers.jsx
+++ src/components/BestSellers.jsx
@@ -1,4 +1,4 @@
-import ProductCard from "./ProductCard";
+import SectionCard from "./SectionCard";
 
 function BestSellers({}) {
   const data = [
@@ -8,18 +8,6 @@ function BestSellers({}) {
       img: "../public/eye-serum.png",
       description: "lorem ipsum",
     },
-    {
-      id: 1,
-      name: "face cleanser",
-      img: "../public/face-cleanser.png",
-      description: "lorem ipsum",
-    },
-    {
-      id: 2,
-      name: "night cream",
-      img: "../public/night-cream.png",
-      description: "lorem ipsum",
-    },
   ];
   return (
     <>
@@ -27,9 +15,9 @@ function BestSellers({}) {
         <h1 className="flex font-bold text-2xl justify-center items-center">
           best sellers
         </h1>
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5gap-4">
           {Math.random() > 0.5 &&
-            data.map((item) => <ProductCard key={item.id} {...item} />)}
+            data.map((item) => <SectionCard key={item.id} {...item} />)}
         </div>
       </div>
     </>
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,21 +4,27 @@ function Category() {
   const data = [
     {
       id: 0,
-      name: "day care",
+      name: "DAY CARE",
       count: 10,
       img: "../public/day.png",
     },
     {
       id: 1,
-      name: "night care",
+      name: "NIGHT CARE",
       count: 2,
       img: "../public/night.png",
     },
     {
-      id: 2,
-      name: "serums",
-      count: 3,
-      img: "../public/serum.png",
+      id: 3,
+      name: "DAY CARE",
+      count: 10,
+      img: "../public/day.png",
+    },
+    {
+      id: 4,
+      name: "NIGHT CARE",
+      count: 2,
+      img: "../public/night.png",
     },
   ];
 
@@ -28,7 +34,7 @@ function Category() {
         <h1 className="flex font-bold text-2xl justify-center items-center">
           categories
         </h1>
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
           {data.map((item) => (
             <CategoryCard
               key={item.id}
