4fd84bd3bbf0835b0ec16a786309b35decb19cff
Michele <mikzed@hotmail.it>
2024-08-05 12:38:34 +0200

    cart created

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,12 @@
 function Cart() {
   return (
     <>
-      {/* <div className="fixed bg-slate-300 h-full w-[500px] right-0 top-0 z-50"></div> */}
+      <div className="fixed bg-slate-100 h-full w-full z-50 sm:w-[500px] sm:right-0 sm:top-0">
+        <div className="flex justify-center items-center">
+          <h1></h1>
+          <img src="logo.png" alt="" />
+        </div>
+      </div>
     </>
   );
 }
