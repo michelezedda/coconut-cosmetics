2beb2205e11edcb6117fc3841d6ec27237c1639e
Michele <mikzed@hotmail.it>
2024-08-08 11:46:47 +0200

    useLocalStorage hook implemented

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -8,7 +8,7 @@ function Cart({ setShowCart }) {
 
   return (
     <>
-      <div className="fixed top-0 left-0 w-full h-screen bg-slate-900/70 z-40 "></div>
+      <div className="fixed top-0 left-0 w-full h-screen bg-slate-900/70 z-40"></div>
       <div className="max-w-[400px] w-full h-auto rounded-bl-md bg-white fixed right-0 top-16 lg:top-20 p-6 z-50">
         <RxCross1
           className="absolute right-0 top-0 m-6 text-[24px] cursor-pointer"
@@ -20,14 +20,19 @@ function Cart({ setShowCart }) {
         <div className="mt-6 max-h-[500px] overflow-y-auto pr-4">
           {cart.length > 0 ? (
             cart.map((item) => (
-              <div key={item.id} className="border-b py-2">
-                <img
-                  src={item.img}
-                  alt={item.name}
-                  className="w-12 h-12 inline-block rounded-sm"
-                />
-                <span className="ml-4">{item.name}</span>
-                <div className="flex justify-end">
+              <div
+                key={item.id}
+                className="border-b py-2 flex items-center justify-between"
+              >
+                <div className="flex items-center">
+                  <img
+                    src={item.img}
+                    alt={item.name}
+                    className="w-12 h-12 rounded-sm"
+                  />
+                  <span className="ml-4">{item.name}</span>
+                </div>
+                <div className="flex items-center">
                   <FiTrash2
                     className="text-lg cursor-pointer mr-4"
                     onClick={() => removeProduct(item.id)}
--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,14 +1,12 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
-// Create the context
 const AppContext = createContext();
 
-// Create a custom hook to use the context
 export const useAppContext = () => useContext(AppContext);
 
-// Create a provider component
 export const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useLocalStorage("cart", []);
   const [showCart, setShowCart] = useState(false);
   const [showProfile, setShowProfile] = useState(false);
   const [shippingPrice, setShippingPrice] = useState(0);
@@ -18,30 +16,42 @@ export const AppProvider = ({ children }) => {
   }, [cart]);
 
   const addToCart = (product) => {
-    setCart((prevCart) => [...prevCart, product]);
+    setCart((prevCart) => {
+      const existingProduct = prevCart.find((item) => item.id === product.id);
+      if (existingProduct) {
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
   };
 
   const removeProduct = (productId) => {
     setCart((prevCart) => {
-      const index = prevCart.findIndex((item) => item.id === productId);
-      if (index !== -1) {
-        return [...prevCart.slice(0, index), ...prevCart.slice(index + 1)];
+      const product = prevCart.find((item) => item.id === productId);
+      if (product.quantity > 1) {
+        return prevCart.map((item) =>
+          item.id === productId
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        );
+      } else {
+        return prevCart.filter((item) => item.id !== productId);
       }
-      return prevCart;
     });
   };
 
   const calculateTotal = () => {
-    return cart.reduce((a, b) => a + b.price, 0);
+    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
   };
 
   const calculateShippingPrice = () => {
     const total = calculateTotal();
-    if (total >= 50 || total === 0) {
-      setShippingPrice(0);
-    } else {
-      setShippingPrice(9.99);
-    }
+    setShippingPrice(total >= 50 || total === 0 ? 0 : 9.99);
   };
 
   return (
