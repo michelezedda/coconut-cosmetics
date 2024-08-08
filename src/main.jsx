bae5e934659d8193fe021cf8b311725390ee225d
Michele <mikzed@hotmail.it>
2024-08-08 10:21:46 +0200

    product page removed

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App.jsx";
-import Product from "./pages/Product";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
