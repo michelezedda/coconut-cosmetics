6d2348e2c522b1cf0454f1c6fa4dd33f96955cdb
Michele <mikzed@hotmail.it>
2025-03-01 12:39:44 +0100

    ticker bug fixed

--- src/components/Ticker.jsx
+++ src/components/Ticker.jsx
@@ -2,7 +2,7 @@ function Ticker() {
   return (
     <>
       <div className="fixed w-full bg-gray-900 text-white py-1">
-        <p className="animate-ticker">
+        <p className="animate-ticker w-max lg:w-full">
           🚚 Free shipping on orders over $50 <span className="px-2">|</span> 📩
           Sign up for our newsletter and get 10% off your first order!
         </p>
