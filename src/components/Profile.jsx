475fad5d60b818ef76fcbd6f5936aa2db7b2b825
Michele <mikzed@hotmail.it>
2025-03-01 13:11:02 +0100

    bug fixing

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import toast from "react-hot-toast";
 
 function Profile() {
+  const handleClick = () => {
+    toast.error(
+      "You can’t proceed, but you can pretend you did. That’s something."
+    );
+  };
   return (
     <>
-      <div className="absolute top-14 right-0 w-[30rem] bg-white rounded-bl-xl text-black p-4">
+      <div className="absolute top-14 right-0 w-[25rem] bg-white rounded-bl-xl text-black p-4">
         <h4 className="text-gray-600 font-semibold text-lg my-4">
           YOUR PROFILE
         </h4>
         <p className="text-gray-600 text-center mb-4">You are not logged in.</p>
         <div className="flex flex-col gap-2">
-          <button className="myButton">LOG IN</button>
-          <button className="myButton">SIGN UP</button>
+          <button className="myButton" onClick={handleClick}>
+            LOG IN
+          </button>
+          <button className="myButton" onClick={handleClick}>
+            SIGN UP
+          </button>
         </div>
       </div>
     </>
--- src/components/Ticker.jsx
+++ src/components/Ticker.jsx
@@ -1,7 +1,7 @@
 function Ticker() {
   return (
     <>
-      <div className="fixed w-full bg-gray-900 text-white py-1">
+      <div className="fixed w-full bg-gray-900 text-white py-1 z-10">
         <p className="animate-ticker w-max lg:w-full">
           🚚 Free shipping on orders over $50 <span className="px-2">|</span> 📩
           Sign up for our newsletter and get 10% off your first order!
