73db711c57351724746f0fdeb5d9b2fccabaf646
Michele <mikzed@hotmail.it>
2025-03-01 12:26:53 +0100

    ticker width modified

--- src/components/Ticker.jsx
+++ src/components/Ticker.jsx
@@ -1,7 +1,7 @@
 function Ticker() {
   return (
     <>
-      <div className="fixed bg-gray-900 text-white py-1">
+      <div className="fixed w-full bg-gray-900 text-white py-1">
         <p className="animate-ticker">
           🚚 Free shipping on orders over $50 <span className="px-2">|</span> 📩
           Sign up for our newsletter and get 10% off your first order!
